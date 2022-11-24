f890bd0704892a78b5fd5086f8b4dbafb882de9b
DELESVAUX Allan <d22018442@A-F001726.salsa.univ-amu.fr>
2022-11-24 15:03:52 +0100

    Affichage du plateau de jeu (couleur et case)

--- ProjetMMNEchecs/src/Affichage.java
+++ ProjetMMNEchecs/src/Affichage.java
@@ -1,11 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Affichage extends JFrame implements ActionListener {
 
+    private JPanel panelMain;
+
     public Affichage(){
+        Echiquier plateau= new Echiquier();
+        plateau.setBounds(10,10,800,800);
+        panelMain= new JPanel();
+        this.add(plateau);
         this.setTitle("Echecs");
+        this.setSize(1200,900);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(null);
     }
 
     @Override
--- ProjetMMNEchecs/src/Echiquier.java
+++ ProjetMMNEchecs/src/Echiquier.java
@@ -1,12 +1,13 @@
-import java.lang.reflect.Array;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Echiquier {
+public class Echiquier extends JComponent {
     private List<Map<Case,Piece>> plateau;
-
     public Echiquier() {
         plateau = new ArrayList<>();
         for(int i=0;i<8;i++){
@@ -21,45 +22,38 @@ public class Echiquier {
         setRois();
         setVides();
     }
-
     public void setPions(){
         for(int i=0;i<8;i++){
             plateau.get(1).put(new Case(i % 2, 1, (char) ('A' + i)), new Pion(0));
             plateau.get(6).put(new Case((i+1) % 2, 6, (char) ('A' + i)), new Pion(1));
         }
     }
-
     public void setTours(){
         plateau.get(0).put(new Case(1, 0, 'A'), new Tour(0));
         plateau.get(0).put(new Case(0, 0, 'H'), new Tour(0));
         plateau.get(7).put(new Case(0, 7, 'A'), new Tour(1));
         plateau.get(7).put(new Case(1, 7, 'H'), new Tour(1));
     }
-
     public void setCavaliers(){
         plateau.get(0).put(new Case(0, 0, 'B'), new Cavalier(0));
         plateau.get(0).put(new Case(1, 0, 'G'), new Cavalier(0));
         plateau.get(7).put(new Case(1, 7, 'B'), new Cavalier(1));
         plateau.get(7).put(new Case(0, 7, 'G'), new Cavalier(1));
     }
-
     public void setFous(){
         plateau.get(0).put(new Case(1, 0, 'C'), new Fou(0));
         plateau.get(0).put(new Case(0, 0, 'F'), new Fou(0));
         plateau.get(7).put(new Case(0, 7, 'C'), new Fou(1));
         plateau.get(7).put(new Case(1, 7, 'F'), new Fou(1));
     }
-
     public void setRois(){
         plateau.get(0).put(new Case(1, 0, 'E'), new Roi(0));
         plateau.get(7).put(new Case(0, 7, 'E'), new Roi(1));
     }
-
     public void setReines(){
         plateau.get(0).put(new Case(0, 0, 'D'), new Reine(0));
-        plateau.get(7).put(new Case(1, 7, 'D'), new Reine(1));
+        plateau.get(7).put(new Case(1, 7, 'D'), new Reine(0));
     }
-
     public void setVides(){
         for(int i=0;i<8;i++){
             for(int j=2;j<6;j++){
@@ -71,6 +65,29 @@ public class Echiquier {
             }
         }
     }
+    public void paintComponent(Graphics g){
+        Graphics2D plateauReel= (Graphics2D) g;
+
+        int tailleCase = 80;
+        boolean couleur=true;
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                if (couleur) {
+                    plateauReel.setPaint(Color.WHITE);
+                } else {
+                    plateauReel.setPaint(Color.BLACK);
+                }
+                plateauReel.fill(new Rectangle2D.Double((j+1)*tailleCase, (i+1)*tailleCase,tailleCase,tailleCase));
+                couleur=!couleur;
+            }
+            couleur=!couleur;
+        }
+        plateauReel.setPaint(Color.BLUE);
+        plateauReel.setStroke(new BasicStroke(2));
+        plateauReel.draw(new Rectangle2D.Double(tailleCase,tailleCase,tailleCase*8,tailleCase*8));
+        plateauReel.dispose();
+    }
+
 }
 
 

20f6754061841c13d0bcf8acc92c387a11fdaa15
DELESVAUX Allan <d22018442@A-F001733.salsa.univ-amu.fr>
2022-12-02 09:06:24 +0100

    JavaDoc Case

--- ProjetMMNEchecs/src/Case.java
+++ ProjetMMNEchecs/src/Case.java
@@ -1,22 +1,53 @@
 import java.util.Objects;
+/***
+ * Classe Case
+ * Definie la position de la case sur un echiquier
+ */
 public class Case {
     private final int posx;
     private final int posy;
+
+    /***
+     * Constructeur de Case
+     * @param posx position de la case en coordonnée x
+     * @param posy position de la case en coordonnée y
+     */
     public Case(int posx, int posy) {
         this.posx = posx;
         this.posy = posy;
     }
+
+    /***
+     * Getter pour la position de la case en coordonnée x
+     * @return int
+     */
     public int getPosx() {
         return posx;
     }
+
+    /***
+     * Getter pour la position de la case en coordonnée y
+     * @return int
+     */
     public int getPosy() {
         return posy;
     }
+
+    /***
+     * Permet d'afficher la position de la case selon ses coordonnées x et y
+     * @return String
+     */
     @Override
     public String toString() {
         return "{"+ posx +", " + posy +
                 '}';
     }
+
+    /***
+     * Permet de comparer savoir si l'objet en attribut est égal à celui appelant
+     * @param o un objet de classe Object
+     * @return boolean
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -24,6 +55,11 @@ public class Case {
         Case aCase = (Case) o;
         return posx == aCase.posx && posy == aCase.posy;
     }
+
+    /***
+     * hashCode
+     * @return int
+     */
     @Override
     public int hashCode() {
         return Objects.hash(posx, posy);

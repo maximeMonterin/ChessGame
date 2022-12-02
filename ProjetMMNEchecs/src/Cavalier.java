bdc1bae8cf9c6d228538e55d41f012331a3464c3
DELESVAUX Allan <d22018442@A-F001733.salsa.univ-amu.fr>
2022-12-02 09:13:44 +0100

    JavaDoc Cavalier

--- ProjetMMNEchecs/src/Cavalier.java
+++ ProjetMMNEchecs/src/Cavalier.java
@@ -1,16 +1,42 @@
 import java.util.ArrayList;
 import java.util.List;
+
+/***
+ * Classe Cavalier héritant de Piece
+ * Definie la Piece comme un Cavalier sur un échiquier
+ */
 public class Cavalier extends Piece{
+    /***
+     * Constructeur de Cavalier
+     * @param couleur couleur du cavalier (0=Blanc; 1=Noir)
+     * @param posx position x sur l'échiquier
+     * @param posy position y sur l'échiquier
+     */
     public Cavalier(int couleur, int posx, int posy) {
         super(new Case(posx, posy), posx, posy, couleur);
     }
+
+    /***
+     * Getter pour recupérer la couleur du Cavalier
+     * @return int
+     */
     public int getCouleur(){
         return this.getCouleurPiece();
     }
+
+    /***
+     * Getter pour récuperer le Nom de la Piece
+     * @return String
+     */
     @Override
     String getNom() {
         return "Cavalier";
     }
+
+    /***
+     * Mouvements correspondant aux cases où pourra aller le cavalier au prochain coup
+     * @return List<Case>
+     */
     @Override
     public List<Case> mouvement() {
 
@@ -53,8 +79,14 @@ public class Cavalier extends Piece{
         }
         return res;
     }
+
+    /***
+     * permet de définir si la piece que l'on souhaite manger est bien de la couleur opposée
+     * @param p piece que l'on veut manger
+     * @return Boolean
+     */
     @Override
-    public Boolean manger(Piece next) {
-        return (couleur != next.getCouleurPiece());
+    public Boolean manger(Piece p) {
+        return (couleur != p.getCouleurPiece());
     }
 }
